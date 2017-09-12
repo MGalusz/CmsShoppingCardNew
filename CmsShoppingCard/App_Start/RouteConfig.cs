b7915a19421733e00f3e4baf3b23eb02fab61dd7
Mgaluszka <Mgaluszka@PLWAWUES107>
2017-09-12 16:49:51 +0200

    layout view for users

--- CmsShoppingCard/App_Start/RouteConfig.cs
+++ CmsShoppingCard/App_Start/RouteConfig.cs
@@ -11,13 +11,22 @@ namespace CmsShoppingCard
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { Controller = "Pages", action = "PagesMenuPartial" }, new[] { "CmsShoppingCard.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { Controller = "Pages", action = "Index" }, new[] { "CmsShoppingCard.Controllers" });
+            routes.MapRoute("Default", "", new { Controller = "Pages", action = "Index" }, new[] { "CmsShoppingCard.Controllers" });
+
+
+            //routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "CmsShoppingCard.Controllers" });
+            //routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "CmsShoppingCard.Controllers" });
+            //routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "CmsShoppingCard.Controllers" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
